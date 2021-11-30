ef6dd20f7be30f923fc4945be12cea9c25ff3795
tyassusanto <tyassusanto>
2021-11-30 15:02:27 +0700

    refactoring

--- w8/search-name.js
+++ w8/search-name.js
@@ -1,18 +1,37 @@
-const students = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol',
-                'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith',
-                'Olivia', 'Penelope']
-let result = []
+// const students = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn',
+//                     'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith','Olivia', 'Penelope']
+// let result = []
 
-const displayResult = (limiter) =>{
-    return limiter
-}
-const filterName = (name, key) =>{
-    return name.filter(res => res.toLowerCase().indexOf(key.toLowerCase())!==-1)
-}
-result = filterName(students,'an');
 
-const limiter = (input, max) => {
-    let result = input.slice(0, max)
-    return result
-}
-console.log(limiter(result,3));
+// const filterName = (name, key) =>{
+//     return name.filter(arr => arr.toLowerCase().indexOf(key.toLowerCase())!==-1)
+//     // return name.filter(arr => arr.toLowerCase().indexOf(key.toLowerCase()))
+// }
+// result = filterName(students,'pe');
+
+// const displayName = (input, max) => {
+//     let result = input.slice(0, max)
+//     return result
+// }
+// console.log(displayName(result,3));
+
+const students = [
+    "Abigail", "Alexandra", "Alison",
+    "Amanda", "Angela", "Bella",
+    "Carol", "Caroline", "Carolyn",
+    "Deirdre", "Diana", "Elizabeth",
+    "Ella", "Faith", "Olivia", "Penelope"];
+  
+  const limitData = (data, limit) => {
+    const dataLimit = data.slice(0, limit);
+    console.log(dataLimit);
+  }
+  
+  const searchName = (keyword, limit, callback) => {
+    const filterData = students.filter((item) => {
+      return item.toLowerCase().indexOf(keyword.toLowerCase()) > -1
+    });
+    callback(filterData, limit);
+  }
+  
+  searchName('an', 3, limitData);
