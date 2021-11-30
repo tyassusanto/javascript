3d6dd1519085194ad3156d485e6c8344d3218eaa
tyassusanto <tyassusanto>
2021-11-30 15:02:15 +0700

    minor-fixing

--- w8/filter-number.js
+++ w8/filter-number.js
@@ -1,21 +1,27 @@
-const sortNumber = (a, b) =>{
+const sortFiltered = (a, b) =>{
     return a-b
 }
 const filterNumber = (nilaiAwal, nilaiAkhir, data) =>{
-    if ((typeof nilaiAwal !== "number") || (typeof nilaiAkhir !== "number")){
-        return `Masukan nilai`
+    if((typeof nilaiAwal !== "number") || (typeof nilaiAkhir !== "number")){
+        return `Input Harus Angka`
     }else if (nilaiAwal > nilaiAkhir){
-        return `Nilai akhir harus lebih besar`
-    }else{
-        let key = data.filter((value) =>{
-            if (value > nilaiAwal && value < nilaiAkhir){
+        return `Nilai akhir harus lebih besar dari nilai awal`
+    }else if(data.length <= 5){
+        return `Minimal panjang array harus 6`
+    }
+    else{
+        let key = data.filter((arr) =>{
+            if (arr > nilaiAwal && arr < nilaiAkhir){
                 return true;
             }else{
                 return false;
             }
         })
-        let filtered = key.sort(sortNumber)
+        let filtered = key.sort(sortFiltered)
         return filtered
     }
 }
-console.log(filterNumber(5, 20, [2, 25, 4, 14, 17, 30, 8]));
+console.log(filterNumber(5, 20 , [2, 25, 4, 14, 17, 30, 8])); // sesuai
+console.log(filterNumber(1, 2 , [2, 25, 4, 14, 17, 30, 8])); // data tidak ada 
+console.log(filterNumber(5, 20 , [2, 25, 4, 14, 17])); // data harus minimal 6
+console.log(filterNumber(21, 20 , [2, 25, 4, 14, 17, 30, 8])); // data awal harus lenih besar
